99966a0e91fa1e1cf237498aa4556e81c389356d
Marshall <itsmoosh@gmail.com>
2020-09-26 01:04:49 -0700

    Added runscript and tuned README for release version

---
+++ go.m
@@ -0,0 +1,7 @@
+%% To generate all profiles and plots for Vance et al 2020 magnetic induction paper
+function go
+    PPEuropa
+    PPGanymede
+    PPCallisto
+    LayeredInductionResponseJupiter
+end
