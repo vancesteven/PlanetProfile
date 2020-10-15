d5fb79bc26cba8e36f918ada50669b7606ab81c4
agmarusiak <41874665+agmarusiak@users.noreply.github.com>
2020-10-15 11:21:02 -0700

    Add files via upload

--- Thermodynamics/WaterIce/CpH2O_Choukroun.m
+++ Thermodynamics/WaterIce/CpH2O_Choukroun.m
@@ -1,11 +1,12 @@
 function Cp_J_kg_K = CpH2O_Choukroun(P_MPa,T_K,ind)
 % from Choukroun and Grasset 2010
 % inds = getInds(P_MPa,T_K,ind);
+% addition for methane clathrates from Ning et al 2014;
 Cp_J_kg_K = getCp2010(T_K,ind);
 
 function Cp_J_kg_K = getCp2010(T_K,inds)
-%  Ice Ih, Ice II, Ice III, Ice V, Ice VI
-c0 = [4190 74.11 2200 820  700 940]; 
-c1 = [9 7.56 0 7  7.56 5.5];
+%  Ice Ih, Ice II, Ice III, Ice V, Ice VI, methane clath
+c0 = [4190 74.11 2200 820  700 940 2150]; 
+c1 = [9 7.56 0 7  7.56 5.5 3.19];
 
 Cp_J_kg_K = polyval([c1(inds) c0(inds)],T_K);
