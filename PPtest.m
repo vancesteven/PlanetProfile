65af33749838b5fbcfd980f5da5d37ee93c82516
Marshall Styczinski <itsmoosh@gmail.com>
2020-12-05 14:43:57 -0800

    Added contribution guidelines and issue templates

---
+++ PPtest.m
@@ -0,0 +1,10 @@
+% Run this function with all CALC_NEW flags in config.m set to 1
+% to test for errors before making a commit.
+function PPtest
+    PPEuropa
+    PPGanymede    
+    PPCallisto
+    PPEnceladus
+    PPTitan
+    MagResponseParamSpaceJupiter
+end
