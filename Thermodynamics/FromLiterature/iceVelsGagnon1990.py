82b5ac7e6b30f6e334990e866be93010e0763032
artlis321 <58958548+artlis321@users.noreply.github.com>
2021-06-10 16:19:33 -0700

    changes thus far

---
+++ Thermodynamics/FromLiterature/iceVelsGagnon1990.py
@@ -0,0 +1,40 @@
+"""
+    Has dependencies to other thermodynamic functions, will return later
+"""
+
+def iceVelsGagnon1990(P,T=None,ind=None):
+    """
+        Parameters:
+        -----------
+        P : float
+            pressure in MPa
+        T (optional) : float
+            temperature in K
+        ind (optional) : int
+            1 <-> liquid (not treated here)
+            2,3,4,5,6 <-> Ice I,II,III,V,VI
+    """
+
+def getV(P,T,ind):
+    return
+
+def getvGagnon(P):
+    """
+        Uses numerical data to find velocities ?????
+
+        Parameters:
+        -----------
+        P : float
+            pressure in MPa
+
+        Returns:
+        --------
+        list
+            list with velocities of ice phases VI and higher
+
+        Notes:
+        ------
+        From Gagnon et. al 1988(I) and 1990
+    """
+    #VIl = 3.914 + 0.000452*P-0.0031*(P*1e-2)**2
+    return
