38ac54f51100b096b08aaeee273d3f957921874b
Artyom <artlis321@gmail.com>
2021-08-04 11:13:47 -0700

    Revert "changes thus far"
    This reverts commit 98064ddb8e29d676285f1e4d9249babd2bcb9d1c.

--- PlanetProfile.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from Thermodynamics.FromLiterature.conductiveMantleTemperature import conductiveMantleTemperature
-from Thermodynamics.FromLiterature.ConvectionDeschampsSotin2001 import ConvectionDeschampsSotin2001
-
-def writeProfile(path,saveStr,header,data):
-    with open(path+saveStr+".txt","w") as f:
-        f.write(header+"\n")
-        for line in data:
-            f.write("\t".join( [ "{:3.5e}".format(val) for val in line] )+"\n")
