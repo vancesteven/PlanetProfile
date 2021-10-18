51bad4b68ad18bcaa69c0fd3b4696126fe16caa6
Marshall Styczinski <itsmoosh@gmail.com>
2021-10-17 20:58:12 -0700

    Added new class version of Matlab struct data organization with Europa example

--- config.py
+++ config.py
@@ -1,107 +1,102 @@
-vsP = 'Porosity_vs_P'
-vsR = 'Porosity_vs_R'
-vperm = 'Permeability'
-vgsks = 'Gs_Ks'
-vseis = 'Seismic'
-vcond = 'Conductivity'
-vgrav = 'Gravity'
-vmant = 'MantleDens'
-vcore = 'CoreMantTrade'
-vpvt4 = 'PTx4'
-vpvt6 = 'PTx6'
-vwedg = 'Wedge'
+"""
+General runtime configuration parameters.
+Overridden by any settings contained within PPBody.py files.
+"""
 
-xtn = '.png'
+from Utilities.dataStructs import *
 
-spicePCK = 'pck00010.tpc'
+Params = ParamsStruct()
 
-# function cfg = config
-#     % Set CALC_NEW options to 0 to re-use profile data when possible.
-#     % It is recommended to keep CALC_NEW=1 except when intermediate parameters
-#     % such as layer thicknesses will not change between runs.
-#
-#     % Runtime options
-#     cfg.CALC_NEW =       1;
-#     cfg.CALC_NEW_REF =   1;
-#     cfg.CALC_NEW_SOUND = 1;
-#     cfg.CALC_NEW_INDUC = 1;
-#     cfg.SKIP_PROFILES =  0; % Whether to skip past all PlanetProfile.m plotting and calculations
-#     cfg.NO_PLOTS =       0;
-#     cfg.HOLD =           1; % Whether to overlay runs when possible
-#     cfg.CONDUCT =        1; % Calculate electrical conductivity
-#     cfg.REDUCED =        1; % Whether to limit number of ocean layers for faster computation of layered induction
-#     cfg.DISP_LAYERS =    0; % Whether to display layer depths and heat fluxes for user
-#     cfg.DISP_TABLES =    0; % Whether to print latex-formatted tables to Matlab command line
-#     cfg.DEPRECATED =     0; % Whether to allow deprecated code to run. Will often cause errors.
-#
-#     % Magnetic induction calculation settings
-#     cfg.DO_EUR = 1;
-#     cfg.DO_GAN = 1;
-#     cfg.DO_CAL = 1;
-#     cfg.DO_ENC = 1;
-#     cfg.DO_MIR = 1;
-#     cfg.DO_ARI = 1;
-#     cfg.DO_PER = 1; % Convert frequency axes to periods
-#     cfg.DO_LEGEND = 1;
-#     cfg.PLOT_FFT = 1; % Whether to show plots of fourier space
-#     cfg.PLOT_CONTOURS = 1; % Contours or surfaces
-#     cfg.PLOT_V2020S = 1; % Mark the selected ocean/conductivity combos used in Vance et al. 2020
-#     cfg.intMethod = 'makima'; % Interpolation method. Certain ones can cause wiggles, notably 'linear'.
-#     cfg.npts_k = 50;
-#     cfg.npts_D = 60;
-#     cfg.np_intp = 200;
-#     cfg.npts_w = 100;
-#     cfg.np_wfine = 1000;
-#     cfg.nIntL = 3; % Number of ocean layers to use when cfg.REDUCED = 1
-#     cfg.opts_odeParams = odeset('RelTol',1e-10,'AbsTol',1e-10,'MaxStep', 2e3,'InitialStep',1e-2);
-#     cfg.opts_odeLayers = odeset('RelTol',1e-8, 'AbsTol',1e-10,'MaxStep',10e3,'InitialStep',1e-2);
-#
-#     % General figure options
-#     cfg.dft_font = 'Arial';
-#     cfg.dft_math = 'STIX Math';
-#     cfg.interpreter = 'tex';
-#     cfg.fig_fmt = '-depsc';
-#     cfg.xtn = '.eps';
-#
-#     % Color selection
-#     cfg.b000ff = [176,  0,255]/255; % b000ff.
-#     % Note the second colons are required to retrieve all 3 RGB elements
-#     % due to Matlab idiosyncracies.
-#     cfg.cmap = parula(100);
-#     cc = 100/256;
-#     cfg.col_contSyn = cfg.cmap(floor(100*cc),:);
-#     cfg.col_contOrb = cfg.cmap(floor( 10*cc),:);
-#     cfg.col_contHrm = cfg.cmap(floor(200*cc),:);
-#
-#     cfg.col_Sw = summer(200); cfg.col_Sw = cfg.col_Sw(51:150,:); % Take bottom half of 'summer' colormap
-#     %cfg.col_Sw = winter(200); cfg.col_Sw = cfg.col_Sw(51:150,:); % Take middle half of 'winter' colormap
-#     cfg.col_coldestSw = 'c'; %cfg.col_Sw(1, :);
-#     cfg.col_midColdSw = cfg.col_Sw(25,:);
-#     cfg.col_middestSw = cfg.col_Sw(50,:);
-#     cfg.col_midWarmSw = cfg.col_Sw(75,:);
-#     cfg.col_warmestSw = cfg.b000ff; %cfg.col_Sw(99,:);
-#     cfg.Sw_alt = [  0,175,238]/255;
-#
-#     cfg.col_MgSO4 = cool(133); cfg.col_MgSO4 = cfg.col_MgSO4(34:133,:); % Take top 3/4 of 'cool' colormap
-#     cfg.col_coldestMgSO4 = 'b'; %cfg.col_MgSO4(1, :);
-#     cfg.col_midColdMgSO4 = cfg.col_MgSO4(25,:);
-#     cfg.col_middestMgSO4 = cfg.col_MgSO4(50,:);
-#     cfg.col_midWarmMgSO4 = cfg.col_MgSO4(75,:);
-#     cfg.col_warmestMgSO4 = cfg.col_MgSO4(99,:);
-#
-#     % Linestyle options
-#     cfg.LS_syn = '-';
-#     cfg.LS_orb = ':';
-#     cfg.LS_hrm = '-.';
-#     cfg.LS_Sw  = '-';
-#     cfg.LS_Mg = '--';
-#     cfg.LS_sp =  ':';
-#     cfg.LW_syn = 2;
-#     cfg.LW_orb = 2;
-#     cfg.LW_hrm = 2;
-#     cfg.LW_sal = 3;
-#     cfg.LW_dil = 1;
-#     cfg.LW_std = 2;
-#     cfg.LW_sound = 1.5;
-#     cfg.LW_seism = 1;
-# end
+Params.lbls.vsP = 'Porosity_vs_P'
+Params.lbls.vsR = 'Porosity_vs_R'
+Params.lbls.vperm = 'Permeability'
+Params.lbls.vgsks = 'Gs_Ks'
+Params.lbls.vseis = 'Seismic'
+Params.lbls.vcond = 'Conductivity'
+Params.lbls.vgrav = 'Gravity'
+Params.lbls.vmant = 'MantleDens'
+Params.lbls.vcore = 'CoreMantTrade'
+Params.lbls.vpvt4 = 'PTx4'
+Params.lbls.vpvt6 = 'PTx6'
+Params.lbls.vwedg = 'Wedge'
+
+Params.spicePCK = 'pck00010.tpc'
+
+Params.CALC_NEW =       1 # Recalculate profiles? If not, read data from disk and re-plot.
+Params.CALC_NEW_REF =   1 # Recalculate reference phase curves?
+Params.CALC_NEW_SOUND = 1 # Recalculate sound speeds?
+Params.CALC_NEW_INDUC = 1 # Recalculate magnetic induction responses?
+Params.SKIP_PROFILES =  0 # Whether to skip past all PlanetProfile.m plotting and calculations
+Params.NO_PLOTS =       0 # Suppress plot creation? If yes, just save profiles to disk.
+Params.HOLD =           1 # Whether to overlay runs when possible
+Params.CONDUCT =        1 # Calculate electrical conductivity
+Params.REDUCED =        1 # Whether to limit number of ocean layers for faster computation of layered induction
+Params.DISP_LAYERS =    0 # Whether to display layer depths and heat fluxes for user
+Params.DISP_TABLES =    0 # Whether to print latex-formatted tables to Matlab command line
+Params.DEPRECATED =     0 # Whether to allow deprecated code to run. Will often cause errors.
+
+# Magnetic induction calculation settings
+Params.DO_EUR = 1 # Calculate responses for Europa?
+Params.DO_GAN = 1 # Ganymede?
+Params.DO_CAL = 1 # And so on...
+Params.DO_ENC = 1
+Params.DO_MIR = 1
+Params.DO_ARI = 1
+Params.DO_PER = 1 # Convert frequency axes to periods
+Params.DO_LEGEND = 1 # Whether to force legends
+Params.PLOT_FFT = 1 # Whether to show plots of fourier space
+Params.PLOT_CONTOURS = 1 # Contours or surfaces
+Params.PLOT_V2020S = 1 # Mark the selected ocean/conductivity combos used in Vance et al. 2020
+Params.intMethod = 'makima' # Interpolation method. Certain ones can cause wiggles, notably 'linear'.
+Params.npts_k = 50 # Resolution for conductivity values in ocean conductivity/thickness plots
+Params.npts_D = 60 # Resolution for ocean thickness as for conductivity
+Params.np_intp = 200 # Number of interpolation points to use for Eckhardt method induction calculations
+Params.npts_w = 100 # Resolution in log frequency space for magnetic excitation spectra
+Params.np_wfine = 1000 # Fine-spacing resolution for log frequency spectrum
+Params.nIntL = 3 # Number of ocean layers to use when REDUCED = 1
+#Params.opts_odeParams = odeset('RelTol',1e-10,'AbsTol',1e-10,'MaxStep', 2e3,'InitialStep',1e-2)
+#Params.opts_odeLayers = odeset('RelTol',1e-8, 'AbsTol',1e-10,'MaxStep',10e3,'InitialStep',1e-2)
+
+# General figure options
+Params.dft_font = 'stix'
+Params.dft_math = 'stix'
+#Params.interpreter = 'tex'
+#Params.fig_fmt = '-depsc'
+Params.xtn = '.eps'
+
+# Color selection
+Params.cmap = 'viridis'
+# Params.col_contSyn = cfg.cmap(floor(100*cc),:)
+# Params.col_contOrb = cfg.cmap(floor( 10*cc),:)
+# Params.col_contHrm = cfg.cmap(floor(200*cc),:)
+
+# Params.col_Sw = summer(200)
+Params.col_coldestSw = 'c'
+# Params.col_midColdSw = cfg.col_Sw(25,:)
+# Params.col_middestSw = cfg.col_Sw(50,:)
+# Params.col_midWarmSw = cfg.col_Sw(75,:)
+Params.col_warmestSw = '#b000ff'
+Params.Sw_alt = (0, 175/255, 238/255)
+
+#Params.col_MgSO4 = cool(133)
+Params.col_coldestMgSO4 = 'b'
+# Params.col_midColdMgSO4 = cfg.col_MgSO4(25,:)
+# Params.col_middestMgSO4 = cfg.col_MgSO4(50,:)
+# Params.col_midWarmMgSO4 = cfg.col_MgSO4(75,:)
+Params.col_warmestMgSO4 = 'xkcd:purpley blue'
+
+# Linestyle options
+Params.LS_syn = '-'
+Params.LS_orb = ':'
+Params.LS_hrm = '-.'
+Params.LS_Sw  = '-'
+Params.LS_Mg = '--'
+Params.LS_sp =  ':'
+Params.LW_syn = 2
+Params.LW_orb = 2
+Params.LW_hrm = 2
+Params.LW_sal = 3
+Params.LW_dil = 1
+Params.LW_std = 2
+Params.LW_sound = 1.5
+Params.LW_seism = 1
