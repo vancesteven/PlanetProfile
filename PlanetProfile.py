9b43d4a097ea6591a70e1870dfe9974d545f008f
Marshall Styczinski <itsmoosh@gmail.com>
2021-10-28 11:16:40 -0700

    Shifted layer propagation function shells to separate file

--- PlanetProfile.py
+++ PlanetProfile.py
@@ -5,6 +5,7 @@ import importlib
 
 # Import all function definitions for this file
 from Utilities.SetupInit import SetupInit, SetupFilenames
+from Thermodynamics.LayerPropagators import IceLayers, OceanLayers, InnerLayers, PlanetDepths
 
 #from Thermodynamics.FromLiterature.conductiveMantleTemperature import conductiveMantleTemperature
 #from Thermodynamics.FromLiterature.ConvectionDeschampsSotin2001 import ConvectionDeschampsSotin2001
@@ -162,74 +163,6 @@ def ReloadProfile(Planet):
     Layers = None
 
     return Planet, Layers
-
-
-def IceLayers(Planet, Layers, Constants):
-    """ Layer propagation from surface downward through ice using geophysics """
-    Layers = IceILayers(Planet, Layers, Constants)
-
-    if Planet.BOTTOM_ICEIII:
-        Layers = IceIIIUnderplateLayers(Planet, Layers, Constants)
-    elif Planet.BOTTOM_ICEV:
-        Layers = IceVUnderplateLayers(Planet, Layers, Constants)
-
-    return Layers
-
-
-def IceILayers(Planet, Layers, Constants):
-    """ Geophysical and thermodynamic calculations for outermost ice layer """
-
-    return Layers
-
-
-def IceIIIUnderplateLayers(Planet, Layers, Constants):
-    """ For cold, thick ice shells, model ice III under ice I layer """
-
-    return Layers
-
-
-def IceVUnderplateLayers(Planet, Layers, Constants):
-    """ For cold, thick ice shells, model ice V and ice III under ice I layer """
-
-    return Layers
-
-
-def OceanLayers(Planet, Layers, Constants):
-    """ Geophysical and thermodynamic calculations for ocean layer """
-
-    return Layers
-
-
-def PlanetDepths(Planet, Layers, Constants):
-    """ Convert from organization by radius into organization by depth """
-
-    return Layers
-
-
-def InnerLayers(Planet, Layers, Constants):
-    """ Geophysical and thermodynamic calculations for silicate and core layers """
-
-    nStepsSilicate, Layers = SilicateLayers(Planet, Layers, Constants)
-    Planet.nStepsTotal = Planet.nStepsHydro + nStepsSilicate
-
-    if Planet.Core.FeCORE:
-        Layers = IronCoreLayers(Planet, Layers, Constants)
-        Planet.nStepsTotal += Planet.nStepsCore
-
-    return Planet, Layers
-
-
-def SilicateLayers(Planet, Layers, Constants):
-    """ Geophysical and thermodynamic calculations for silicate layers """
-    nStepsSilicate = 0
-
-    return nStepsSilicate, Layers
-
-
-def IronCoreLayers(Planet, Layers, Constants):
-    """ Geophysical and thermodynamic calculations for core layers """
-
-    return Layers
 
 
 if __name__ == '__main__': main()
