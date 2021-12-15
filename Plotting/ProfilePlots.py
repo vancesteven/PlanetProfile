c4a8d635abdce1f81b3c90bae3ce78799585d701
mniesyt <maraniesyt@gmail.com>
2021-12-14 16:26:17 -0800

    added wedge plot options to config.py and wedge plot to ProfilePlots

--- Plotting/ProfilePlots.py
+++ Plotting/ProfilePlots.py
@@ -5,6 +5,7 @@ import math
 from matplotlib.patches import Wedge
 from matplotlib.collections import PatchCollection
 
+
 def GeneratePlots(Planet, Params):
 
     if Params.PLOT_GRAVITY: PlotGravPres(Planet, Params)
@@ -29,7 +30,7 @@ def PlotGravPres(Planet, Params):
     plt.ylabel('$r_\mathrm{' + Planet.name + '}$')
 
     plt.subplots_adjust(wspace = 0.5)
-    plt.suptitle('Gravity and Pressure')
+    plt.suptitle('Gravity and pressure')
     plt.show()
 
 def PlotHydrosphereProps(Planet, Params):
@@ -49,7 +50,7 @@ def PlotHydrosphereProps(Planet, Params):
     plt.xlabel('Temperature (K)')
     plt.ylabel('Depth (km)')
 
-    plt.suptitle('Hydrosphere Properties')
+    plt.suptitle('Hydrosphere properties')
     plt.subplots_adjust(wspace=0.5)
     plt.show()
 
@@ -67,55 +68,53 @@ def PlotTradeoff(Planet, Params):
 
 
 def PlotWedge(Planet, Params):
-    data = {'r': Planet.r_m/1000,
-            'phase': Planet.phase}
     fig, ax = plt.subplots()
     width = (math.pi / 7)*180/math.pi
     patches = []
     colors = []
-    #length = len(Planet.r_m)
-    #widths = []
     iPhaseTrans = 1+np.argwhere(Planet.phase[1:] != Planet.phase[:-1])
     iPhaseTrans = np.insert(iPhaseTrans, 0, 0) # this makes sure the ice phase is included
-    #widths = iPhaseTrans/length
+    #iPhaseTrans = np.insert(iPhaseTrans, int([Planet.Bulk.R_m]))  # this makes sure the ice phase is included
+    print(iPhaseTrans)
+    for layerPhase in iPhaseTrans:
+        if Planet.phase[layerPhase] == 0:
+            colors.append(Params.Colors.OceanTop)
+        elif Planet.phase[layerPhase] == 1:
+            colors.append(Params.Colors.IceI)
+        elif Planet.phase[layerPhase] == 2:
+            colors.append(Params.Colors.IceII)
+        elif Planet.phase[layerPhase] == 3:
+            colors.append(Params.Colors.IceIII)
+        elif Planet.phase[layerPhase] == 5:
+            colors.append(Params.Colors.IceV)
+        elif Planet.phase[layerPhase] == 6:
+            colors.append(Params.Colors.IceVI)
+        elif Planet.phase[layerPhase] == 30:
+            colors.append(Params.Colors.Clath)
+        elif Planet.phase[layerPhase] == 50:
+            colors.append(Params.Colors.Rock)
+        elif Planet.phase[layerPhase] == 100:
+            colors.append(Params.Colors.Core)
+    print(colors)
     phases = [Planet.phase[iShell] for iShell in iPhaseTrans]
     radii = [Planet.r_m[iShell]/Planet.Bulk.R_m for iShell in iPhaseTrans]
     #print(phases)
-    #print(radii)
-    patches = [Wedge((0.5,0), radii[1], 90 - width, 90 + width, 1-radii[0], color='b') ]
-    #for i in range(len(radii)):
-       # patches += [Wedge((0.5,0), radii[i], 90 - width, 90 + width, 1-radii[i-1])]
+    print(radii)
+    for i in range(len(radii)):
+        patches += [Wedge((0.5,0), radii[i], 90 - width, 90 + width, lw = 0.25)]#, #1-radii[i-1] )]#color = colors[i])]
     print(patches)
     p = PatchCollection(patches)
-    #colors = colors.append(Params.Colors.IceI)
 
-    #p.set_array(colors)
+    p.set_color(colors)
+    p.set_edgecolor('k')
     ax.add_collection(p)
-    plt.title('Interior Wedge Diagram')
-    plt.suptitle('$Tb_K = \mathrm{' + str(Planet.Bulk.Tb_K) + '}$')
+    ax.set_aspect('equal')
+
+    #fig.colorbar(p, ax = ax)
+    plt.suptitle('Interior wedge diagram')
+    plt.title('$T_b = ' + str(Planet.Bulk.Tb_K) + '\,\mathrm{K}' + ', Composition = ' + str(Planet.Ocean.comp) + ', Salinity = ' + str(Planet.Ocean.wOcean_ppt) + '\,\mathrm{ppt}$')
+    plt.margins(0.02)
     plt.show()
-'''
-    for layerPhase in iPhaseTrans:
-        if layerPhase == 0:
-            colors.append(Params.Colors.OceanTop)
-        elif layerPhase == 1:
-            colors.append(Params.Colors.IceI)
-        elif layerPhase == 2:
-            colors.append(Params.Colors.IceII)
-        elif layerPhase == 3:
-            colors.append(Params.Colors.IceIII)
-        elif layerPhase == 5:
-            colors.append(Params.Colors.IceV)
-        elif layerPhase == 6:
-            colors.append(Params.Colors.IceVI)
-        elif layerPhase == 30:
-            colors.append(Params.Colors.Clath)
-        elif layerPhase == 50:
-            colors.append(Params.Colors.Rock)
-        elif layerPhase == 100:
-            colors.append(Params.Colors.Core)
-    print(colors)
-'''
 
 
 
