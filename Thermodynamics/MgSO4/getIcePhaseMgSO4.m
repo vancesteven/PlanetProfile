416754c62f30b3ee6cf0f06b866b480ec1157bba
Marshall "Moosh" Styczinski <itsmoosh@gmail.com>
2022-01-15 10:39:27 -0800

    Corrected log/unit errors in Margules MgSO4 phases
    The activity coefficient y (gamma) had been calculated with xH2O multiplied outside ln(y), when it should be ln(y*x), and in calculating y, the ideal gas constant R had been scaled to be in J/kg/K instead of J/mol/K, which gave units to the argument of the exponential.

--- Thermodynamics/MgSO4/getIcePhaseMgSO4.m
+++ Thermodynamics/MgSO4/getIcePhaseMgSO4.m
@@ -20,8 +20,8 @@ phase(phase==4) = 5;
 
 function dmu = deltaMuILiqMgSO4(P,T,x)
 R = 8.314;
-dmu = MuIceLiqH2O(P,T) - R./0.018.*T.*(activity(P,T,x,-1.8e6,150,1.45e-4,-12,246)*x);% this line contains the coefficients from Vance et al. 2014 for MgSO4
+dmu = MuIceLiqH2O(P,T) - R./0.018.*T.*log(activity(P,T,x,-1.8e6,150,1.45e-4,-12,246)*x);% this line contains the coefficients from Vance et al. 2014 for MgSO4
 
 function y = activity(P,T,x,w0,w1,w2,w3,To)
-R = 8.314./0.018;
-y = (((1-x).^2).*w0.*(1+w1.*tanh(w2.*P)).*(1+w3./(T-To)^2)./(R.*T));
+R = 8.314;
+y = exp(((1-x).^2).*w0.*(1+w1.*tanh(w2.*P)).*(1+w3./(T-To).^2)./(R.*T));
