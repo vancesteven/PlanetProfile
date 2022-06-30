8250b5c255df3be15c8bdc8ef4d5536adcb16870
Marshall Styczinski <itsmoosh@gmail.com>
2022-06-29 20:54:28 -0700

    Updated version numbers to v2.2

--- PlanetProfile/Utilities/PPversion.py
+++ PlanetProfile/Utilities/PPversion.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version
 # Current PlanetProfile version tag
-ppVerNum = '2.2.0-dev'
+ppVerNum = '2.2.0'
 # Compatible version tag numbers
 compatNums = {
     'seafreeze': '0.9.2',
