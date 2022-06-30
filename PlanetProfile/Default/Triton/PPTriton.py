53d62a66b5080f11c68f73d49f4acb79628c77fb
Marshall Styczinski <itsmoosh@gmail.com>
2022-06-29 20:49:40 -0700

    Created default python model for Triton and temp. removed clathrates from pluto

--- PlanetProfile/Default/Triton/PPTriton.py
+++ PlanetProfile/Default/Triton/PPTriton.py
@@ -1,5 +1,5 @@
 """
-PPPluto
+PPTriton
 Contains all body-specific parameters and information for PlanetProfile models of this body.
 Import as a module and access information assigned to the attributes of the Planet struct.
 Note that this file expects to be imported from the directory above.
@@ -7,28 +7,19 @@ Note that this file expects to be imported from the directory above.
 import numpy as np
 from PlanetProfile.Utilities.defineStructs import PlanetStruct, Constants
 
-Planet = PlanetStruct('Pluto')
+Planet = PlanetStruct('Triton')
 
 # Reduce search range for melting pressure for smaller body
 Planet.PfreezeUpper_MPa = 150
 
 """ Bulk planetary settings """
-Planet.Bulk.R_m = 1195.0e3
-Planet.Bulk.M_kg = 1.314e22
-Planet.Bulk.Tsurf_K = 44
+Planet.Bulk.R_m = 1353.4e3
+Planet.Bulk.M_kg = 2.140e22
+Planet.Bulk.Tsurf_K = 38
 Planet.Bulk.Psurf_MPa = 0.0
-Planet.Bulk.Cmeasured = 0.31  # Based on rough forward models with fixed layer densities in Hussmann et al. (2006): https://doi.org/10.1016/j.icarus.2006.06.005
+Planet.Bulk.Cmeasured = 0.31  # Copied from Pluto model, based on rough forward models with fixed layer densities in Hussmann et al. (2006): https://doi.org/10.1016/j.icarus.2006.06.005 . These authors could not find a fit for Triton.
 Planet.Bulk.Cuncertainty = 0.03
-Planet.Bulk.Tb_K = 265.0  # Thickness <100 km is unlikely re: Kamata
-
-# Clathrate underplating currently does not function properly.
-# A future update will implement clathrate underplating for Pluto.
-Planet.Do.CLATHRATE = False
-Planet.Steps.nClath = 30
-Planet.Bulk.clathType = 'bottom'
-Planet.Bulk.clathMaxThick_m = 10.0e3  # Estimate based on Kamata et al. (2019): https://doi.org/10.1038/s41561-019-0369-8
-Planet.Bulk.qSurf_Wm2 = 10.0e-3  # Maximum of ~13 mW/m^2 based on Conrad et al. (2021): https://doi.org/10.1029/2020JE006641
-# Highly porous surface ice for top ~5 km re: Kamata
+Planet.Bulk.Tb_K = 266.0
 
 """ Layer step settings """
 Planet.Steps.nIceI = 200
@@ -38,7 +29,7 @@ Planet.Steps.iSilStart = Planet.Steps.nIceI
 
 """ Hydrosphere assumptions/settings """
 Planet.Ocean.comp = 'MgSO4'
-Planet.Ocean.wOcean_ppt = 50
+Planet.Ocean.wOcean_ppt = 10
 Planet.Ocean.deltaP = 0.25
 Planet.Ocean.deltaT = 0.1
 Planet.Ocean.PHydroMax_MPa = 250.0
@@ -57,3 +48,7 @@ Planet.Do.Fe_CORE = False
 
 """ Seismic properties of solids """
 Planet.Seismic.lowQDiv = 1.0
+
+""" Magnetic induction """
+Planet.Magnetic.ionosBounds_m = [250e3, 450e3]  # Approximation based on Tyler et al. (1989): https://doi.org/10.1126/science.246.4936.1466
+Planet.Magnetic.sigmaIonosPedersen_Sm = [1e-16, 0.05]
