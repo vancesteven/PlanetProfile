ae537267c911682404cae813a5362e813d7bbfaf
Marshall Styczinski <itsmoosh@gmail.com>
2022-11-07 04:28:19 -0800

    Pared down Europa inductogram example file

--- PlanetProfile/Default/Europa/PPEuropaInductOgram.py
+++ PlanetProfile/Default/Europa/PPEuropaInductOgram.py
@@ -14,12 +14,12 @@ Planet.PfreezeUpper_MPa = 150
 
 """ Bulk planetary settings """
 Planet.Bulk.R_m = 1560.8e3
-Planet.Bulk.M_kg = 4.7991e22
+Planet.Bulk.M_kg = 4.800e22
 Planet.Bulk.Tsurf_K = 110
 Planet.Bulk.Psurf_MPa = 0.0
 Planet.Bulk.Cmeasured = 0.346
 Planet.Bulk.Cuncertainty = 0.005
-Planet.Bulk.Tb_K = 269.2
+Planet.Bulk.Tb_K = 268.305
 
 """ Layer step settings """
 Planet.Steps.nIceI = 100
@@ -45,12 +45,6 @@ Planet.Do.POROUS_ROCK = False
 # Mantle equation of state model
 Planet.Sil.mantleEOS = 'CV3hy1wt_678_1.tab'  # (2900 for Q= 100 GW, 3240 for Q= 220 GW)
 Planet.Sil.rhoSilWithCore_kgm3 = 3539.0  # This is the 1-bar, 275 K value from CV3hy1wt_678_1.tab
-# Planet.Sil.mantleEOS = 'Simple_CI_HS_green_PP.tab'  # CI chondrite material minus Fe core, computed with Green et al. 2016 (JMG) solution model and Lodders and Fegley 1998
-# Planet.Sil.rhoSilWithCore_kgm3 = 2975
-# Planet.Sil.mantleEOS = 'Simple_CM_HS_green_PP.tab'  # CM chondrite material minus Fe core, computed with Green et al. 2016 (JMG) solution model and Lodders and Fegley 1998
-# Planet.Sil.rhoSilWithCore_kgm3 = 2975
-# Planet.Sil.mantleEOS = 'Simple_CV_HS_green_PP.tab'  # CV chondrite material minus Fe core, computed with Green et al. 2016 (JMG) solution model and Lodders and Fegley 1998
-# Planet.Sil.rhoSilWithCore_kgm3 = 2975
 
 """ Core assumptions """
 Planet.Do.Fe_CORE = True
@@ -72,23 +66,3 @@ Planet.Seismic.lowQDiv = 1.0
 """ Magnetic induction """
 Planet.Magnetic.SCera = 'Galileo'
 Planet.Magnetic.extModel = 'JRM33C2020'
-#Planet.Magnetic.ionosBounds_m = 100e3
-#Planet.Magnetic.sigmaIonosPedersen_Sm = 30/100e3
-
-# The block below should be made into one single function that returns the FTdata struct if the file is found, and warns the user/downloads if not.
-# try:
-#     load(['FTdata' Planet.name],'FTdata')
-# catch:
-#     dlNow = FTdataMissingWarning()
-#     if dlNow; load(['FTdata' Planet.name],'FTdata'); end
-
-""" Interior constraints imposed in Vance et al. 2014 """
-# Planet.Sil.mSi = 28.0855
-# Planet.Sil.mS = 32.065
-# Planet.Sil.mFe = 55.845
-# Planet.Sil.mMg = 24.305
-# Planet.Sil.xOl = 0.44  # Percentage of olivine - Javoy (1995) - Fe/Si = 0.909 Mg/Si = 0.531, Mg# = 0.369
-# Planet.Sil.xSi = (Planet.Sil.xOl+2*(1-Planet.Sil.xOl))*Planet.Sil.mSi/(Planet.Sil.xOl*184.295+(1-Planet.Sil.xOl)*244.3805) # mass fraction of sulfur in silicates
-# Planet.Sil.MEarth_kg = 5.97e24
-# Planet.Sil.xSiEarth = 0.1923  # Javoy in kg/kg in Exoplanets paper20052006-xSiSolar only in mole
-# Planet.Sil.xK = 1.0  # Enrichment in K
