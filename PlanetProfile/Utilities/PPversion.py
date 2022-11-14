f0a826f8a8fdd85106f6a2f97c5963e2b6563d41
Marshall Styczinski <itsmoosh@gmail.com>
2022-11-14 05:10:26 -0800

    Updated to 2.3.3 to include missing mantle comp

--- PlanetProfile/Utilities/PPversion.py
+++ PlanetProfile/Utilities/PPversion.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version
 # Current PlanetProfile version tag
-ppVerNum = '2.3.2'
+ppVerNum = '2.3.3'
 # Compatible version tag numbers
 compatNums = {
     'seafreeze': '0.9.2',
--- Utilities/PPversion.m
+++ Utilities/PPversion.m
@@ -1,3 +1,3 @@
 function vernum = PPversion
-    vernum = '2.3.2';
+    vernum = '2.3.3';
 end
