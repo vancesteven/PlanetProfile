39f88f39307ea84f2a8ab704b777c0e64dccddf8
Marshall Styczinski <itsmoosh@gmail.com>
2023-05-03 20:04:05 -0700

    Changed default behavior of PPuninstall to confirm deletion of non-default files

--- PlanetProfile/install.py
+++ PlanetProfile/install.py
@@ -1,5 +1,8 @@
 import os, shutil
 from glob import glob
+
+import numpy as np
+
 from PlanetProfile import _ROOT, _Defaults, _SPICE, configTemplates, configLocals, CopyOnlyIfNeeded
 
 def PPinstall():
@@ -35,24 +38,74 @@ def PPinstall():
         CopyOnlyIfNeeded(repoSpice, localSpice)
 
 
-def PPuninstall():
-    """ Removes the same contents as what is added by PPinstall.
+def PPuninstall(KEEP_NEW=None):
     """
-    print('WARNING: This will remove all files within Body folders, the SPICE folder, the folders themselves, and configPP files.')
-    answer = input('Continue? (y/n) ')
+    Removes the same contents as what is added by PPinstall.
+    If any directory would then be empty, the directory is deleted.
+
+    Args:
+        KEEP_NEW: If True, retain any files the user has created atop the default install.
+            If False, remove everything in the Body/ and SPICE/ directories. Defaults to None (prompt user).
+
+    Returns:
+        None
+    """
+    print('WARNING: This will remove all files within Body folders, the SPICE folder, the folders themselves, and configPP files ' +
+        'that have been copied from the PlanetProfile defaults.')
+    answer = input('Continue? (y/N) ')
 
     if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
-        print('\nRemoving all Body folders, SPICE kernels, and configPP files.')
-        Bodies = [os.path.basename(bodyDir) for bodyDir in glob(os.path.join(_Defaults, '*'))]
-        [shutil.rmtree(Body, ignore_errors=True) for Body in Bodies]
-        shutil.rmtree(os.path.basename(_SPICE), ignore_errors=True)
+        print('\nRemoving all copied Body folder contents, SPICE kernels, and configPP files.')
+
+        # Remove config files
         [os.remove(cfg) for cfg in configLocals]
 
-    elif answer in ['n', 'N', 'no', 'No', 'NO']:
+        Bodies = [os.path.basename(bodyDir) for bodyDir in glob(os.path.join(_Defaults, '*'))]
+        # Remove PPBody.py files
+        Defaults = glob(os.path.join(_Defaults, '*', 'PP*.py'))
+        PPfiles = [file.split(f'Default{os.sep}')[-1] for file in Defaults]
+        [os.remove(file) for file in PPfiles]
+
+        # Remove induction files
+        inductionDefaults = glob(os.path.join(_Defaults, '*', 'inductionData', '*.txt'))
+        inductionLocal = [file.split(f'Default{os.sep}')[-1] for file in inductionDefaults]
+        inductionDirs = [os.path.join(Body, 'inductionData') for Body in Bodies]
+        [os.remove(file) for file in inductionLocal]
+
+        # Remove SPICE files
+        spiceDefaults = glob(os.path.join(_SPICE, '*'))
+        spiceLocal = [file.split(f'{_ROOT}{os.sep}')[-1] for file in spiceDefaults]
+        spiceDir = os.path.basename(_SPICE)
+        [os.remove(file) for file in spiceLocal]
+
+        # Check if each Body folder (and the SPICE folder) is empty, and if so, delete it.
+        # Otherwise, warn user and ask to delete
+        if not os.listdir(spiceDir):
+            os.rmdir(spiceDir)
+        else:
+            if KEEP_NEW is None:
+                print(f'{spiceDir} contains non-default files.')
+                answer = input('Delete them anyway? (y/N) ')
+                if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
+                    shutil.rmtree(os.path.basename(_SPICE), ignore_errors=True)
+            elif not KEEP_NEW:
+                shutil.rmtree(os.path.basename(_SPICE), ignore_errors=True)
+        for Body, inductDir in zip(Bodies, inductionDirs):
+            if not os.listdir(inductDir):
+                os.rmdir(inductDir)
+            if not os.listdir(Body):
+                os.rmdir(Body)
+            else:
+                if KEEP_NEW is None:
+                    print(f'{Body} dir contains non-default files.')
+                    answer = input('Delete them anyway? (y/N) ')
+                    if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
+                        shutil.rmtree(Body, ignore_errors=True)
+                elif not KEEP_NEW:
+                    shutil.rmtree(Body, ignore_errors=True)
+    else:
         print('Aborting.')
         exit(0)
-    else:
-        raise ValueError(f'Response "{answer}" not recognized.')
 
 
 if __name__ == '__main__':
