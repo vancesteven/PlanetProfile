68c1783a020bd68d01a28cd2326f6bb89c3251a8
Steve Vance <vance.steven@gmail.com>
2024-10-09 12:43:48 -0700

    Update PlanetProfile.m to at GPL3 copyright statement
    Signed-off-by: Steve Vance <vance.steven@gmail.com>

--- PlanetProfile.m
+++ PlanetProfile.m
@@ -1,6 +1,11 @@
 function outPlanet = PlanetProfile(Planet,Seismic,Params)
-%% PlanetProfile
-%
+% PlanetProfile, Software framework for constructing interior structure models based on planetary properties. 
+% Copyright (C) 2018  Steven D. Vance
+% This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by 
+% the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+% This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
 % Based on
 % S. Vance, M. Bouffard, M. Choukroun, and C. Sotin.
 % Ganymede's internal structure including thermodynamics of magnesium sulfate oceans in contact with ice.
